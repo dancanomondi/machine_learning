08d0ba980987b85b9beb7546119fb5c71f19a99d
dancanomondi <dancan.omondi6831.com>
2022-11-12 10:48:41 +0300

    updatd the folders

---
+++ machine_learning/car_predictions/car_acceptability.py
@@ -0,0 +1,21 @@
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+import pickle
+
+# Load dataset
+url = "https://raw.githubusercontent.com/dancanomondi/machine_learning/main/machine_learning/car.data"
+names = ["buying", "maint", "doors", "persons", "lug_boot", "safety"]
--- machine_learning/iris_predictions/iris.py
+++ machine_learning/iris_predictions/iris.py
@@ -23,12 +23,6 @@ url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
-# summarize the data
-# Load dataset
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = read_csv(url, names=names)
-# shape
 # print(dataset.shape)
 # # head
 # print(dataset.head(20))
